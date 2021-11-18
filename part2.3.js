d7d9bf48f699dd0b93022865c8f8f6aea5230a2c
Jyothsna <94163611+JyothsnaVellampalli@users.noreply.github.com>
2021-11-18 11:44:11 +0530

    Update part2.3.js

--- part2.3.js
+++ part2.3.js
@@ -1,6 +1,5 @@
-var new_string = “”;
- 
-for (var i = 11; i > 0; i -- ) {
- new_string += numsArr[i] + “ “ 
+ var new_string = "";
+ for (var i = 11; i > 0; i -- ) {
+ new_string += i+" "
 }
 console.log(new_string.trim());
