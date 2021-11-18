8e91a28b4d3b88004d0274ac748203b57b39d034
Jyothsna <94163611+JyothsnaVellampalli@users.noreply.github.com>
2021-11-18 12:00:30 +0530

    Update part2.2.js

--- part2.2.js
+++ part2.2.js
@@ -1,7 +1,8 @@
 var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-var new_string = “”;
+var new_string = "";
  
-for (var i = 0; i < numArr.length; i++) {
+for (var i = 0; i < numsArr.length-1; i++) {
  new_string += numsArr[i]+"," 
 }
-console.log(new_string.trim());
+new_string += numsArr[numsArr.length-1]
+console.log(new_string)
